75a892655eb98739b20177c92c233f5cdedb69f2
George Skudder <george.skudder@hotmail.com>
2025-07-01 07:27:51 +0100

    Added basic game loop + showing actors and items

--- game.py
+++ game.py
@@ -6,13 +6,28 @@ class Scene:
         self.items = items
         self.dirs = dirs
 class Actor:
-    def __init__(self,name,desc,con):
+    def __init__(self,name,desc,con,shown):
         self.name = name
         self.desc = desc
         self.con = con
+        self.shown = shown
 class Item:
-    def __init__(self,name,desc,cpu):
+    def __init__(self,name,desc,shown):
         self.name = name
         self.desc = desc
-        self.cpu = cpu #can pick up
-wmap = {"TEST":Scene("TEST ROOM","You are in the test room. If you somehow got here, your game is broken!",{"OM":Actor("Old Man","It is an old man.",{"INIT":["Hello. Would you like a cup of tea?","",{"Yes":["YOU PASSED THE TEST","Tea",{}],"No":["YOU FAILED","",{}]}]})},{"RolPap":Item("Rolled-up Newspaper","It is a rolled-up Sunday Times.",True),"Tea":Item("Tea","A lukewarm cup of tea.",False)},{"N":["disgust","NOT EXISTENT"]})}
+        self.shown = shown
+
+croom = "TEST" #current room
+wmap = {"TEST":Scene("TEST ROOM","You are in the test room. This room is used to test work-in-progress features.",{"OM":Actor("Old Man","It is an old man.",{"INIT":["Hello. Would you like a cup of tea?","",{"Yes":["YOU PASSED THE TEST","Tea",{}],"No":["YOU FAILED","",{}]}]},True)},{"RolPap":Item("Rolled-up Newspaper","It is a rolled-up Sunday Times.",True),"Tea":Item("Tea","A lukewarm cup of tea.",False,)},{"N":["disgust","NOT EXISTENT"]})}
+while True:
+    rd = wmap[croom] #room data
+    print(rd.desc + "\n\nYou see:")
+    for i in rd.actors:
+        obj = rd.actors[i]
+        if obj.shown:
+            print(obj.name)
+    for i in rd.items:
+        obj = rd.items[i]
+        if obj.shown:
+            print(obj.name)
+    com = input("\n> ").lower() #command
